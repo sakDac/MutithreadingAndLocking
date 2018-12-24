37fe9df3a81d3fec0d3c1ffea92db7c4c98e17db
saket bhushan <saket.bhushan46@gmail.com>
2018-12-24 09:21:17 +0530

    OperationQueue Operations.

--- MutithreadingAndLocking/MultiThreadingOperationQueue.swift
+++ MutithreadingAndLocking/MultiThreadingOperationQueue.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
+let imgUrl = "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg"
+
 class MultiThreadingOperationQueue {
-    
     let url = "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg"
-    
     func downloadImageInOperation() {
         let operationQueue = OperationQueue()
         for i in 1 ... 5 {
@@ -23,7 +23,45 @@ class MultiThreadingOperationQueue {
         }
         operationQueue.maxConcurrentOperationCount = 1
         print("no of queue :: \(operationQueue.maxConcurrentOperationCount)")
-//        operationQueue.waitUntilAllOperationsAreFinished() // This is similiar to SYNC in dispatch => it will freeze the app.
+        operationQueue.waitUntilAllOperationsAreFinished() // This is similiar to SYNC in dispatch => it will freeze the app.
+    }
+    
+    
+    func downloadImageInOperationUsingCustomOperations() {
+        
+        let operationQueue = OperationQueue()
+
+        let operationFirst = OperationFirst()
+
+        let operationSecond = OperationSecond()
+        operationSecond.addDependency(operationFirst)
+
+        operationQueue.addOperations([operationFirst, operationSecond], waitUntilFinished: false)
+        
+    }
+    
+}
+
+
+class OperationFirst: Operation {
+    
+    override func main() {
+        for i in 1 ... 5 {
+                let url = URL(string: imgUrl)
+                _ = try? Data(contentsOf: url!)
+            print("image downloaded : operation one : \(i)")
+        }
+    }
+}
+
+class OperationSecond: Operation {
+    
+    override func main() {
+        for i in 1 ... 5 {
+            let url = URL(string: imgUrl)
+            _ = try? Data(contentsOf: url!)
+            print("image downloaded : operation Two : \(i)")
+        }
     }
     
 }
